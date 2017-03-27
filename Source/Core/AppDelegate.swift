5fa9a627398536d089e4af851cbb5d61bda2d2db
David Jennes <david.jennes@appwise.be>
2017-03-27 19:58:52 +0200

    fix issue with obj-c selectors and swift generics name mangling

--- Source/Core/AppDelegate.swift
+++ Source/Core/AppDelegate.swift
@@ -48,6 +48,48 @@ open class AppDelegate<ConfigType: Config>: UIResponder, UIApplicationDelegate {
 
 		return true
 	}
+	
+	// Empty definitions to avoid selector mismatch due to generic name mangling
+	
+	open func applicationDidBecomeActive(_ application: UIApplication) {}
+	open func applicationWillResignActive(_ application: UIApplication) {}
+	open func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool { return true }
+	open func applicationDidReceiveMemoryWarning(_ application: UIApplication) {}
+	open func applicationWillTerminate(_ application: UIApplication) {}
+	open func applicationSignificantTimeChange(_ application: UIApplication) {}
+	open func application(_ application: UIApplication, willChangeStatusBarOrientation newStatusBarOrientation: UIInterfaceOrientation, duration: TimeInterval) {}
+	open func application(_ application: UIApplication, didChangeStatusBarOrientation oldStatusBarOrientation: UIInterfaceOrientation) {}
+	open func application(_ application: UIApplication, willChangeStatusBarFrame newStatusBarFrame: CGRect) {}
+	open func application(_ application: UIApplication, didChangeStatusBarFrame oldStatusBarFrame: CGRect) {}
+	open func application(_ application: UIApplication, didRegister notificationSettings: UIUserNotificationSettings) {}
+	open func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {}
+	open func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {}
+	open func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {}
+	open func application(_ application: UIApplication, didReceive notification: UILocalNotification) {}
+	open func application(_ application: UIApplication, handleActionWithIdentifier identifier: String?, for notification: UILocalNotification, completionHandler: @escaping () -> Swift.Void) {}
+	open func application(_ application: UIApplication, handleActionWithIdentifier identifier: String?, forRemoteNotification userInfo: [AnyHashable : Any], withResponseInfo responseInfo: [AnyHashable : Any], completionHandler: @escaping () -> Swift.Void) {}
+	open func application(_ application: UIApplication, handleActionWithIdentifier identifier: String?, forRemoteNotification userInfo: [AnyHashable : Any], completionHandler: @escaping () -> Swift.Void) {}
+	open func application(_ application: UIApplication, handleActionWithIdentifier identifier: String?, for notification: UILocalNotification, withResponseInfo responseInfo: [AnyHashable : Any], completionHandler: @escaping () -> Swift.Void) {}
+	open func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Swift.Void) {}
+	open func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Swift.Void) {}
+	open func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Swift.Void) {}
+	open func application(_ application: UIApplication, handleEventsForBackgroundURLSession identifier: String, completionHandler: @escaping () -> Swift.Void) {}
+	open func application(_ application: UIApplication, handleWatchKitExtensionRequest userInfo: [AnyHashable : Any]?, reply: @escaping ([AnyHashable : Any]?) -> Swift.Void) {}
+	open func applicationShouldRequestHealthAuthorization(_ application: UIApplication) {}
+	open func applicationDidEnterBackground(_ application: UIApplication) {}
+	open func applicationWillEnterForeground(_ application: UIApplication) {}
+	open func applicationProtectedDataWillBecomeUnavailable(_ application: UIApplication) {}
+	open func applicationProtectedDataDidBecomeAvailable(_ application: UIApplication) {}
+	open func application(_ application: UIApplication, shouldAllowExtensionPointIdentifier extensionPointIdentifier: UIApplicationExtensionPointIdentifier) -> Bool { return true }
+	open func application(_ application: UIApplication, viewControllerWithRestorationIdentifierPath identifierComponents: [Any], coder: NSCoder) -> UIViewController? { return nil }
+	open func application(_ application: UIApplication, shouldSaveApplicationState coder: NSCoder) -> Bool { return false }
+	open func application(_ application: UIApplication, shouldRestoreApplicationState coder: NSCoder) -> Bool { return false }
+	open func application(_ application: UIApplication, willEncodeRestorableStateWith coder: NSCoder) {}
+	open func application(_ application: UIApplication, didDecodeRestorableStateWith coder: NSCoder) {}
+	open func application(_ application: UIApplication, willContinueUserActivityWithType userActivityType: String) -> Bool { return true }
+	open func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Swift.Void) -> Bool { return true }
+	open func application(_ application: UIApplication, didFailToContinueUserActivityWithType userActivityType: String, error: Error) {}
+	open func application(_ application: UIApplication, didUpdate userActivity: NSUserActivity) {}
 }
 
 private extension AppDelegate {
