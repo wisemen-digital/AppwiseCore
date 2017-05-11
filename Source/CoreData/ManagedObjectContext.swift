2f86e074155504b49ef5fd8c16fc93ec10c375c5
David Jennes <david.jennes@appwise.be>
2017-05-11 14:16:53 +0200

    DB: add some helper methods for fetching items

---
+++ Source/CoreData/ManagedObjectContext.swift
@@ -0,0 +1,73 @@
+//
+//  ManagedObjectContext.swift
+//  AppwiseCore
+//
+//  Created by David Jennes on 06/03/2017.
+//  Copyright © 2016 Appwise. All rights reserved.
+//
+
+import CoreData
+import SugarRecord
+
+public extension NSManagedObjectContext {
+	enum Error: Swift.Error {
+		case entityNotFound
+		case unsupportedIdentityAttributes
+		case unableToCastObject(to: Any)
+	}
+	
+	public func first<T: Entity>(value: Any) throws -> T? {
+		guard let entity = T.self as? NSManagedObject.Type else { throw StorageError.invalidType }
+		guard let description = NSEntityDescription.entity(forEntityName: entity.entityName, in: self) else {
+			throw Error.entityNotFound
+		}
+		let keys = description.keysForUniquing
+		guard keys.count == 1, let key = keys.first else {
+			throw Error.unsupportedIdentityAttributes
+		}
+		
+		return first(key: key, value: value)
+	}
+	
+	public func first<T: Entity>(key: String, value: Any) -> T? {
+		let predicate = NSPredicate(format: "%K = %@", argumentArray: [key, value])
+		return first(predicate: predicate)
+	}
+	
+	public func first<T: Entity>(sortDescriptor: NSSortDescriptor? = nil, predicate: NSPredicate? = nil) -> T? {
+		let request = FetchRequest<T>(self, sortDescriptor: sortDescriptor, predicate: predicate, fetchLimit: 1)
+		
+		guard let result = try? request.fetch() else { return nil }
+		return result.first
+	}
+	
+	public func inContext<T: NSManagedObject>(_ item: T) throws -> T {
+		if item.objectID.isTemporaryID {
+			try item.managedObjectContext?.obtainPermanentIDs(for: [item])
+		}
+		
+		guard let result = try existingObject(with: item.objectID) as? T else {
+			throw Error.unableToCastObject(to: T.self)
+		}
+		return result
+	}
+}
+
+extension NSEntityDescription {
+	fileprivate var keysForUniquing: Set<String> {
+		var keys = Set<String>()
+		var entity: NSEntityDescription? = self
+		
+		while (entity != nil) {
+			if let identity = entity?.userInfo?["identityAttributes"] as? String {
+				identity.components(separatedBy: CharacterSet(charactersIn: " ,"))
+					.filter { !$0.isEmpty }
+					.forEach { keys.insert($0) }
+			}
+			
+			entity = entity?.superentity
+		}
+			
+		return keys
+	}
+}
