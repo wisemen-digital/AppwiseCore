0a27f3c7988a128e1d4df54cc6d4fe23f8162e53
David Jennes <david.jennes@appwise.be>
2017-07-26 02:11:03 +0200

    replace sugarrecord stuff with our own

--- Source/CoreData/Database.swift
+++ Source/CoreData/Database.swift
@@ -6,13 +6,25 @@
 //  Copyright © 2016 Appwise. All rights reserved.
 //
 
+import CocoaLumberjack
 import CoreData
 import SugarRecord
+
+private struct Observer {
+	weak var moc: NSManagedObjectContext?
+	let tokens: [NSObjectProtocol]
+}
 
 public final class DB: NSObject {
 	internal static let shared = DB()
 	
 	private var db: CoreDataDefaultStorage? = nil
+	private var observers = [Observer]()
+	
+	private var root: NSManagedObjectContext {
+		guard let main = db?.mainContext as? NSManagedObjectContext, let moc = main.parent else { fatalError("Core Data not initialized") }
+		return moc
+	}
 	
 	public static var main: NSManagedObjectContext {
 		guard let moc = shared.db?.mainContext as? NSManagedObjectContext else { fatalError("Core Data not initialized") }
@@ -20,33 +32,84 @@ public final class DB: NSObject {
 	}
 	
 	public static var save: NSManagedObjectContext {
-		guard let moc = shared.db?.saveContext as? NSManagedObjectContext else { fatalError("Core Data not initialized") }
+		let moc = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+		moc.parent = shared.root
+		
+		let willSave = NotificationCenter.default.addObserver(forName: NSNotification.Name.NSManagedObjectContextWillSave, object: moc, queue: nil, using: { [weak moc] (notification) in
+			guard let s = moc, s.insertedObjects.count > 0 else { return }
+			_ = try? s.obtainPermanentIDs(for: Array(s.insertedObjects))
+		})
+		let didSave = NotificationCenter.default.addObserver(forName: NSNotification.Name.NSManagedObjectContextDidSave, object: moc, queue: .main, using: { (notification) -> Void in
+			DB.main.mergeChanges(fromContextDidSave: notification as Notification)
+		})
+		
+		shared.cleanupObservers()
+		shared.observers += [Observer(moc: moc, tokens: [willSave, didSave])]
+		
 		return moc
 	}
 	
 	public typealias SaveBlock = () -> Void
 	@discardableResult public static func operation<T>(_ operation: @escaping (_ context: NSManagedObjectContext, _ save: @escaping SaveBlock) throws -> T) throws -> T {
-		guard let db = shared.db else { fatalError("Core Data not initialized") }
-		return try db.operation() { (context, save) throws in
-			try operation(context as! NSManagedObjectContext, save)
+		let context = save
+		var _error: Error!
+		var returnedObject: T!
+		
+		context.performAndWait {
+			do {
+				returnedObject = try operation(context, { () -> Void in
+					do {
+						try context.save()
+					}
+					catch {
+						_error = error
+					}
+					shared.root.performAndWait({
+						if shared.root.hasChanges {
+							do {
+								try shared.root.save()
+							} catch {
+								_error = error
+							}
+						}
+					})
+				})
+			} catch {
+				_error = error
+			}
 		}
+		if let error = _error {
+			throw error
+		}
+		
+		return returnedObject
 	}
 	
 	public typealias SaveBlockWitCallback = (@escaping (Error?) -> Void) -> Void
 	public static func backgroundOperation(_ operation: @escaping (_ context: NSManagedObjectContext, _ save: @escaping SaveBlockWitCallback) -> ()) {
-		guard let db = shared.db else { fatalError("Core Data not initialized") }
+		let context = save
+		var _error: Error!
 		
-		var _completion: ((Error?) -> Void)? = nil
-		db.backgroundOperation({ (context, save) in
-			operation(context as! NSManagedObjectContext, { (callback) in
-				_completion = callback
-				save()
+		context.perform {
+			operation(context, { (callback) in
+				do {
+					try context.save()
+				}
+				catch {
+					_error = error
+				}
+				shared.root.perform {
+					if shared.root.hasChanges {
+						do {
+							try shared.root.save()
+						}
+						catch {
+							_error = error
+						}
+					}
+					callback(_error)
+				}
 			})
-		}) { (error) in
-			guard let completion = _completion else { return }
-			DispatchQueue.main.async {
-				completion(error)
-			}
 		}
 	}
 	
@@ -90,7 +153,25 @@ public final class DB: NSObject {
 	}
 	
 	@objc internal func reset() {
-		try? db?.removeStore()
+		do {
+			try db?.removeStore()
+		} catch let error {
+			DDLogError("Error deleting DB store: \(error)")
+		}
 		db = nil
+	}
+	
+	private func cleanupObservers() {
+		var active: [Observer] = []
+		
+		for observer in observers {
+			if observer.moc == nil {
+				observer.tokens.forEach { NotificationCenter.default.removeObserver($0) }
+			} else {
+				active += [observer]
+			}
+		}
+		
+		observers = active
 	}
 }
